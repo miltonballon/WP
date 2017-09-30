1d9412be675bb106736c60689fb4a81b3144b3ef
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-09-29 22:45:13 -0400

    Clase Connection implementada junto con la bd

---
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/Connection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace SistemaGestorDeInformes
+{
+    class Connection
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        private OleDbCommand command; //para realizar consultas
+        private OleDbDataAdapter da;    //para guardar los datos 
+        //string ruta = connection.ConnectionString.ToString();
+        public Connection()
+        {
+            
+        }
+        public void connect()
+        {
+            try
+            {
+                connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database/Database.accdb;
+                Persist Security Info=False;";
+          
+                connection.Open();
+                MessageBox.Show("Conectado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se conecto con la BD");
+            }
+        }
+    }
+}
