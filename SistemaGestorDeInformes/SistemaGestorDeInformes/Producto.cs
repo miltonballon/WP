fd230c3465ece6dc9de5249fe6152e06a273055a
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-10-01 19:54:31 -0400

    Clase producto creada

---
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestorDeInformes
+{
+    class Producto
+    {
+        private string nombre;
+        private string unidad;
+        private string proveedor;
+
+        public void setNombre(string n) { nombre = n; }
+        public void setUnidad(string u) { unidad = u; }
+        public void setProveedor(string p) { proveedor = p; }
+        public string getNombre() { return nombre;}
+        public string getUnidad() { return unidad; }
+        public string getProveedor() { return proveedor; }
+    }
+
+}
