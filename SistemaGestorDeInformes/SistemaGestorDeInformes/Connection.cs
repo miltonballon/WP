ebb45168f02b35dd4549a06c2b9ca7d48a1c51aa
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-10-02 22:27:36 -0400

    Clase controlador terminada con algunas impleentaciones sobre la clase connection y Producto

--- SistemaGestorDeInformes/SistemaGestorDeInformes/Connection.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/Connection.cs
@@ -8,7 +8,6 @@ using System.Data.OleDb;
 using System.Windows.Forms;
 using System.IO;
 namespace SistemaGestorDeInformes
-
 {
     class Connection
     {
@@ -16,34 +15,72 @@ namespace SistemaGestorDeInformes
         private OleDbCommand command; //para realizar consultas
         private OleDbDataAdapter da;    //para guardar los datos 
 
-
+        public OleDbCommand getCommmand() { return command; }
 
         public Connection()
         {
-            
+
         }
         public void connect()
         {
             try
             {
                 connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database/Database.accdb;
-                Persist Security Info=False;";      
+                Persist Security Info=False;";
                 connection.Open();
                 //MessageBox.Show("Conectado");
             }
             catch (Exception ex)
             {
+
                 MessageBox.Show("No se conecto con la BD");
             }
         }
+        public int buscarYDevolverId(string consulta)
+        {
+            int respuesta = -1;
+            string searchQuery = consulta;
+            executeQuery(searchQuery);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    respuesta = reader.GetInt32(0);
+                }
+            }
+            return respuesta;
 
-        public OleDbDataAdapter ejecutarConsulta(string consulta)
+        }
+
+        public OleDbDataAdapter executeQuery(string consulta)
         {
             command = new OleDbCommand();
             command.Connection = connection;
             command.CommandText = consulta;
             da = new OleDbDataAdapter(command);
             return da;
+        }
+        public void executeInsertion(string consulta)
+        {
+
+            command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = consulta;
+            command.ExecuteNonQuery();
+
+
+        }
+        public void connectionOpen()
+        {
+            connection.Open();
+        }
+        public void connectionClose()
+        {
+            connection.Close();
+        }
+        public void clearTextBox()
+        {
+            da = null;
         }
     }
 }
