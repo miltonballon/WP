f78e1eb85d25ef397d796656a14d551680961654
jp <jprodriguez60@gmail.com>
2017-10-03 08:57:08 -0400

    Creado clase controladorFactura

---
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/ControladorFactura.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestorDeInformes
+{
+    class ControladorFactura
+    {
+        public Connection c = new Connection();
+        public ControladorFactura()
+        {
+            c.connect();
+        }
+
+        public void insertarFactura(Factura factura)
+        {
+            int nFactura = factura.getNFactura(),
+                nAutorizacion = factura.getNAutorizacion(),
+                nit = factura.getNit(),
+                idProveedor = buscarProveedor(factura.getProveedor()); ;
+            DateTime fecha = factura.getFecha();
+            String consulta = "INSERT INTO FACTURA(n_factura, n_autorizacion, id_proveedor, nit, fecha) VALUES (";
+            consulta += nFactura+", ";
+            consulta += nAutorizacion + ", ";
+            consulta += idProveedor + ", ";
+            consulta += nit + ", ";
+            consulta += "#"+fecha.ToString("dd/MM/yyyy")+"#)";
+            //MessageBox.Show(consulta);
+            c.connectionOpen();
+            c.executeInsertion(consulta);
+            c.connectionClose();
+        }
+
+        public int buscarProveedor(String nombreProveedor)
+        {
+            String consulta = "select id FROM Proveedor where Proveedor = '" + nombreProveedor + "'";
+            int id = c.buscarYDevolverId(consulta);
+            c.connectionClose();
+            return id;
+        }
+
+    }
+}
