655b75975fd8232f8eb97f8ee50b8c9c99b7397d
Pedro <pedroandresmg@gmail.com>
2017-10-08 16:48:50 -0400

    Prototipo interfaz configuracion de informes

--- SistemaGestorDeInformes/SistemaGestorDeInformes/ShowProducts.Designer.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/ShowProducts.Designer.cs
@@ -197,8 +197,9 @@
             // informeToolStripMenuItem
             // 
             this.informeToolStripMenuItem.Name = "informeToolStripMenuItem";
-            this.informeToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.informeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.informeToolStripMenuItem.Text = "Informe";
+            this.informeToolStripMenuItem.Click += new System.EventHandler(this.informeToolStripMenuItem_Click);
             // 
             // abrirTrimestreToolStripMenuItem
             // 
