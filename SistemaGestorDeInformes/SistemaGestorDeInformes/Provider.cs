5fcbaa7cd2f6b7f6dc752c4d39915b56864317f2
jp <jprodriguez60@gmail.com>
2017-10-08 09:22:48 -0400

    Cambios menores al script de la BD, creado la clase Provider y se inicio
    la implementacion de ProviderController, cambios menores a InterfazRegistrarFactura

---
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/Provider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestorDeInformes
+{
+    class Provider
+    {
+        private String name;
+        private int nit;
+        private List<Product> products;
+
+        public Provider(string name, int nit, List<Product> products)
+        {
+            this.name = name;
+            this.nit = nit;
+            this.products = products;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getNit()
+        {
+            return nit;
+        }
+
+        public List<Product> getProducts()
+        {
+            return products;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setNit(int nit)
+        {
+            this.nit = nit;
+        }
+        public void setProducts(List<Product> products)
+        {
+            this.products = products;
+        }
+    }
+}
