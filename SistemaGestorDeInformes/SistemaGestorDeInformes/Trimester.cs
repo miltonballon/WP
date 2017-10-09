2ad8b43e67e946b4805e3cb6c9bd9da25ac1175a
jp <jprodriguez60@gmail.com>
2017-10-09 18:30:32 -0400

    Creado clase Trimester

---
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/Trimester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestorDeInformes
+{
+    class Trimester
+    {
+        private bool open;
+        private String name;
+
+        public Trimester(bool open, string name)
+        {
+            this.open = open;
+            this.name = name;
+        }
+
+        public bool isOpen()
+        {
+            return open;
+        }
+
+        public void setOpen(bool open)
+        {
+            this.open = open;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+    }
+}
