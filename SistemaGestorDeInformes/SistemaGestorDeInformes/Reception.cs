dd0067b64fa5b5d51dd7b281ecd99eca6b4ee07d
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-10-15 02:04:08 -0400

    Interfaz de recepcion de Viveres creada

--- SistemaGestorDeInformes/SistemaGestorDeInformes/Reception.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/Reception.cs
@@ -8,5 +8,39 @@ namespace SistemaGestorDeInformes
 {
     class Reception
     {
+        private Product product;
+        private string expirationDate;
+        private string receptionDate;
+        private int total;
+        public Reception()
+        {
+
+
+
+        }
+        public Product Product
+        {
+            get { return Product; }
+            set { Product = value; }
+        }
+        public string ExpirationDate
+        {
+            get { return expirationDate; }
+            set { expirationDate = value; }
+        }
+        public string Provider
+        {
+            get { return receptionDate; }
+            set { receptionDate = value; }
+        }
+        public int Unit
+        {
+            get { return total; }
+            set { total = value; }
+        }
+
+        
+        
+        
     }
 }
