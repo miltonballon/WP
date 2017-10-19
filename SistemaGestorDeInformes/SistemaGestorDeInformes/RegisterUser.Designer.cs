1938854a83adfd822dc5ca71d95d968da02cbd31
Stan777 <e_vargas.v@outlook.com>
2017-10-19 19:29:14 -0400

    haciendo referencia a controlador

--- SistemaGestorDeInformes/SistemaGestorDeInformes/RegisterUser.Designer.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/RegisterUser.Designer.cs
@@ -31,6 +31,7 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtName
@@ -54,16 +55,27 @@
             this.txtEmail.Size = new System.Drawing.Size(100, 20);
             this.txtEmail.TabIndex = 2;
             // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(453, 199);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
             // RegisterUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 261);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtName);
             this.Name = "RegisterUser";
             this.Text = "RegisterUser";
+            this.Load += new System.EventHandler(this.RegisterUser_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,5 +86,6 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
--- SistemaGestorDeInformes/SistemaGestorDeInformes/RegisterUser.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/RegisterUser.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace SistemaGestorDeInformes
 {
+    
     public partial class RegisterUser : Form
     {
+        UserController userController;
         public RegisterUser()
         {
             InitializeComponent();
+            userController = new UserController();
+        }
+
+        private void RegisterUser_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
