30097c38c60205445e4ed816d05ab45afa2a997f
jp <jprodriguez60@gmail.com>
2017-10-23 17:51:32 -0400

    agregado clase ReportController

---
+++ SistemaGestorDeInformes/ControllerLibrary/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestorDeInformes
+{
+    class ReportController
+    {
+        private Connection c;
+        private ReportSheetController reportSheetController;
+        public ReportController()
+        {
+            c = new Connection();
+            c.connect();
+            reportSheetController = new ReportSheetController();
+        }
+    }
+}
