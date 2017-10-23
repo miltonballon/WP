20ad8ce1d4ce940c47a0427845021e762b2d0689
Stan777 <e_vargas.v@outlook.com>
2017-10-23 19:44:25 -0400

    Corrección mejoramiento de funcionamiento FirtsSetup

--- SistemaGestorDeInformes/SistemaGestorDeInformes/FirtsSetup.Designer.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/FirtsSetup.Designer.cs
@@ -71,6 +71,7 @@
             this.Name = "FirtsSetup";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Presentación de Sistema";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FirtsSetup_FormClosing);
             this.Load += new System.EventHandler(this.FirtsSetup_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
