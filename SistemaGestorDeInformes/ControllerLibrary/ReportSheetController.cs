fd7e2d568bbcc98f871988cb13f165f8e1eeabf8
jp <jprodriguez60@gmail.com>
2017-10-23 17:12:21 -0400

    Creado clase ReportSheetController

---
+++ SistemaGestorDeInformes/ControllerLibrary/ReportSheetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestorDeInformes
+{
+    class ReportSheetController
+    {
+        private Connection c;
+        public ReportSheetController()
+        {
+            c = new Connection();
+            c.connect();
+        }
+
+
+    }
+}
