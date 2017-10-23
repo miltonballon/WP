c1888fa626530c5530e109c979d56cf8378bbf95
jp <jprodriguez60@gmail.com>
2017-10-23 18:04:11 -0400

    agregado metodo GetReportById en ReportController

--- SistemaGestorDeInformes/ControllerLibrary/ReportController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/ReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EntityLibrary;
+using System.Data.SQLite;
 
 namespace SistemaGestorDeInformes
 {
@@ -34,6 +35,34 @@ namespace SistemaGestorDeInformes
         {
             String query = "SELECT id FROM Report WHERE id_trimester=" + idTrimester + " AND name='" + name + "'";
             return c.FindAndGetID(query);
+        }
+
+        public Report GetReportById(int id)
+        {
+            Report report = null;
+            String query = "SELECT * FROM Report WHERE id=" + id;
+            SQLiteDataReader data = c.query_show(query);
+            if (data.Read())
+            {
+                String name = data[1].ToString();
+                DateTime date = getDate(data[2].ToString());
+                List<ReportSheet> reportSheets = reportSheetController.GetAllReportSheetsByReportId(id);
+                report = new Report(id, name, date, reportSheets);
+            }
+            data.Close();
+            c.dataClose();
+            return report;
+        }
+
+        private DateTime getDate(String dateString)
+        {
+            DateTime date;
+            string[] dates = dateString.Split('/');
+            int day = Int32.Parse(dates[0])
+                , month = Int32.Parse(dates[1])
+                , year = Int32.Parse(dates[2]);
+            date = new DateTime(year, month, day);
+            return date;
         }
     }
 }
