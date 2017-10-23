29bf8b4fcb1813281f8986eccea0cc017341c45c
jp <jprodriguez60@gmail.com>
2017-10-23 16:47:44 -0400

    metodo insertReportSheetCell implementado

--- SistemaGestorDeInformes/ControllerLibrary/ReportSheetCellController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/ReportSheetCellController.cs
@@ -17,9 +17,21 @@ namespace SistemaGestorDeInformes
             c.connect();
         }
 
-        public int insertReportSheetCell(ReportSheetCell reportSheetCell)
+        public int insertReportSheetCell(ReportSheetCell reportSheetCell, int idReporSheet)
         {
+            int row = reportSheetCell.Row,
+                column = reportSheetCell.Column;
+            String content = reportSheetCell.Content;
+            String query = "INSERT INTO Report_sheet_cell(id_report_sheet, row, column, content) VALUES("
+                +idReporSheet+", "+row+", "+column+" ,'"+content+"')";
+            c.executeInsertion(query);
+            return getIdByUniqueFields(idReporSheet,row,column);
+        }
 
+        public int getIdByUniqueFields(int idReporSheet, int row, int column)
+        {
+            String query = "SELECT id FROM Report_sheet_cell WHERE id_report_sheet="+idReporSheet+" AND row="+row+" AND column="+column;
+            return c.FindAndGetID(query);
         }
 
     }
