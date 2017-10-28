111bbdf8d3e5ab603e8bcfebc467b5d4affda526
jp <jprodriguez60@gmail.com>
2017-10-28 10:04:49 -0400

    corregiso errores de compilacion

--- SistemaGestorDeInformes/ControllerLibrary/ConfigurationController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/ConfigurationController.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using EntityLibrary;
 
-namespace SistemaGestorDeInformes
+namespace ControllerLibrary
 {
     public class ConfigurationController
     {
