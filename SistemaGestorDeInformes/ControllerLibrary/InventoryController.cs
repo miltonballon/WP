6468fe706f17369059e47017ad3f9915488a1e2e
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-10-31 02:23:14 -0400

    Cambios en metodo para cambiar valores de stock

--- SistemaGestorDeInformes/ControllerLibrary/InventoryController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/InventoryController.cs
@@ -32,15 +32,7 @@ namespace ControllerLibrary
             }
             
         }
-        public void ModifyInventory(int id, OutputReception reception)
-        {
-            
-                int total = restTotalToProduct(findId(id), reception.Total);
-                //MessageBox.Show("asd" + findId(id) + " " + total);
-                updateReception(findId(id), total);
-         
-
-        }
+        
         public List<Inventory> getAllProducts()
         {
             List<Inventory> receptions = new List<Inventory>();
