3bffc07c998e46f79a4a893b70c0d0c736f3e12d
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-10-31 18:11:22 -0400

    Codigo inservible eliminado en ShowInventory

--- SistemaGestorDeInformes/SistemaGestorDeInformes/ShowInventory.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/ShowInventory.cs
@@ -26,7 +26,7 @@ namespace SistemaGestorDeInformes
 
         private void pantallaPrincipalToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Main principal = new Main();//para volver atras
+            Main principal = new Main();
             principal.WindowState = this.WindowState;
             this.Hide();
             principal.Show();
