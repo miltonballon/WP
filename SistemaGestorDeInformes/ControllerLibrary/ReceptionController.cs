daff2621171621433eab4ab567a57d42e8b20712
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-10-31 18:23:34 -0400

    Refactorizacion en inventoryController

--- SistemaGestorDeInformes/ControllerLibrary/ReceptionController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/ReceptionController.cs
@@ -84,11 +84,7 @@ namespace ControllerLibrary
             {
                 Product prod = new Product();
                 
-                Reception p = new Reception();
-                //MessageBox.Show("d"+data[0].ToString()+"B"+data[1].ToString()+"C"+Int32.Parse(data[2].ToString()));
-                prod.Name = data[0].ToString();
-                prod.Unit = data[1].ToString();
-                p.Product = prod;
+               
 
                 p.Total =Int32.Parse(data[2].ToString());
                 reception.Add(p);
