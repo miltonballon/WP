ed723d0e1c14084c47c66cad2038916b76b095b4
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-11-06 00:17:21 -0400

    Constructor inservible eliminado

--- SistemaGestorDeInformes/SistemaGestorDeInformes/ModifyProduct.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/ModifyProduct.cs
@@ -15,18 +15,39 @@ namespace SistemaGestorDeInformes
     public partial class ModifyProduct : Form
     {
         ProductController pc;
-        Product product;
+        public Product product;
+        int idProduct;
         public ModifyProduct()
         {
             InitializeComponent();
             pc = new ProductController();
             product = new Product();
+            //registerClassProduct();
+            idProduct = pc.searchProduct(product);
+           
+        }
+        public ModifyProduct(Product p)
+        {
+            InitializeComponent();
+            pc = new ProductController();
+            product = p;
+            idProduct = pc.searchProduct(product);
+            MessageBox.Show("" + product.Name + product.Clasification + product.Provider + product.Unit);
         }
         public void registerClassProduct()
         {
             product.Name = Product_TextBox.Text;
             product.Provider = Provider_TextBox.Text;
             product.Unit = Unit_TextBox.Text;
+            if (FreshRadioButton.Checked == true)
+            {
+                product.Clasification = "Fresco";
+            }
+            else
+            {
+                product.Clasification = "Seco";
+            }
+
         }
         private void atrasButton_Click(object sender, EventArgs e)
         {
@@ -46,18 +67,25 @@ namespace SistemaGestorDeInformes
         private void FreshRadioButton_CheckedChanged(object sender, EventArgs e)
         {
             product.Clasification = "Fresco";
+          
+           
         }
 
         private void DryRadioButton_CheckedChanged(object sender, EventArgs e)
         {
             product.Clasification = "Seco";
+         
         }
 
         private void Registrar_Button_Click(object sender, EventArgs e)
         {
             registerClassProduct();
+            
+            
             pc.insertProduct(product);
-            pc.addReferencesToTableProduct_Provider_Unit(product);
+            MessageBox.Show("" + product.Name + product.Clasification + product.Provider + product.Unit);
+            pc.updateProduct(product,idProduct);
+
             MessageBox.Show("Editado exitosamente");
             ShowProducts main = new ShowProducts();
             main.WindowState = this.WindowState;
