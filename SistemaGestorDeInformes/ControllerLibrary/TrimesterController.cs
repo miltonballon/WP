7ac633837b733c06760c3b65d11e5953820ecff4
jp <jprodriguez60@gmail.com>
2017-11-06 23:22:03 -0400

    agregado metodo GetLastTwoTrimester en trimesterController

--- SistemaGestorDeInformes/ControllerLibrary/TrimesterController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/TrimesterController.cs
@@ -26,18 +26,55 @@ namespace ControllerLibrary
             c.executeInsertion(query);
         }
 
+        public List<Trimester> GetLastTwoTrimester()
+        {
+            List<Trimester> trimesters = new List<Trimester>();
+            String query = "SELECT * FROM trimester WHERE id=(SELECT MAX(id) FROM trimester)";
+            SQLiteDataReader data = c.query_show(query);
+            int id=0;
+            if (data.Read())
+            {
+                id = Int32.Parse(data[0].ToString());
+            }
+            data.Close();
+            c.dataClose();
+            for (int i = 1; i <= 2; i++)
+            {
+                Trimester trimester = GetTrimesterById(id - i);
+                if (trimester != null)
+                {
+                    trimesters.Add(trimester);
+                }
+            }
+            return trimesters;
+        }
+
+        public Trimester GetTrimesterById(int id)
+        {
+            Trimester trimester = null;
+            String query = "SELECT * FROM trimester WHERE id="+id;
+            SQLiteDataReader data = c.query_show(query);
+            if (data.Read())
+            {
+                String name = data[2].ToString();
+                int bit = Int32.Parse(data[1].ToString());
+                trimester = new Trimester(id, name);
+                trimester.SetOpen(bit == 1);
+            }
+            data.Close();
+            c.dataClose();
+            return trimester;
+        }
+
         public Trimester GetLastTrimester()
         {
             Trimester trimester=null;
             String query = "SELECT * FROM trimester WHERE id=(SELECT MAX(id) FROM trimester)";
             SQLiteDataReader data = c.query_show(query);
             if (data.Read())
-            {
-                String name = data[2].ToString();
-                int bit = Int32.Parse(data[1].ToString()), 
-                    id = Int32.Parse(data[0].ToString());
-                trimester = new Trimester(id,name);
-                trimester.SetOpen(bit==1);
+            { 
+                int id = Int32.Parse(data[0].ToString());
+                trimester = GetTrimesterById(id);
             }
             data.Close();
             c.dataClose();
