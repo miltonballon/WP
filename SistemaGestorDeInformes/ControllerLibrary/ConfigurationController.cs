2d3ecbb55ab252e75f09d4ff4d3d65ed94a10660
jp <jprodriguez60@gmail.com>
2017-11-12 08:58:57 -0400

    ConfigurationController usando command para los queries

--- SistemaGestorDeInformes/ControllerLibrary/ConfigurationController.cs
+++ SistemaGestorDeInformes/ControllerLibrary/ConfigurationController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using EntityLibrary;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.Data;
 
 namespace ControllerLibrary
 {
     public class ConfigurationController
     {
-        public Connection c = new Connection();
-
+        public Connection c;
+        private SQLiteConnection connectionString;
         public ConfigurationController()
         {
+            c = new Connection();
             c.connect();
+            connectionString = c.ConnectionString;
         }
 
         public Configuration GetConfiguration()
@@ -38,26 +41,23 @@ namespace ControllerLibrary
 
         public void insertConfiguration(Configuration configuration)
         {
-            /*
-            String query = "INSERT INTO configuration(scholarships, n_departure, days_left, minimum_quantity) VALUES(";
+            String query = "INSERT INTO configuration(scholarships, n_departure, days_left, minimum_quantity) VALUES(@schollar, @n_depar, @days, @minimum)";
             int scholarships = configuration.getScholarships(),
                 nDeparture = configuration.getNDeparture(),
                 nDaysLeft=configuration.getNDaysLeft(),
                 minimumQuantity=configuration.getMinimumQuantity();
-            query += scholarships + ", " + nDeparture + ","+nDaysLeft+","+minimumQuantity+")";
-            c.executeInsertion(query);
-            */
 
-            try
-            {
-                String Query = "Insert into [Configuration] (scholarships, n_departure, days_left,minimum_quantity) values ('" + configuration.getScholarships() + "', '" + configuration.getNDeparture() + "', '" + configuration.getNDaysLeft() + "', '" + configuration.getMinimumQuantity()+ "')";
-                c.executeInsertion(Query);
-                
+            SQLiteCommand command = new SQLiteCommand(query, connectionString);
+            command.Parameters.Add("@schollar", DbType.String);
+            command.Parameters.Add("@n_depar", DbType.String);
+            command.Parameters.Add("@days", DbType.String);
+            command.Parameters.Add("@minimum", DbType.String);
+            command.Parameters["@schollar"].Value = scholarships;
+            command.Parameters["@n_depar"].Value = nDeparture;
+            command.Parameters["@days"].Value = nDaysLeft;
+            command.Parameters["@minimum"].Value = minimumQuantity;
 
-            }
-            catch (Exception e)
-            {
-            }
+            c.executeInsertion(command);
         }
     }
 }
