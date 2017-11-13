023e55b23b2785aa518741789273b7fdabbe0d9c
Christian Flores Prieto <christian_flores995@hotmail.com>
2017-11-12 23:40:40 -0400

    Contenido de label cambiado

--- SistemaGestorDeInformes/SistemaGestorDeInformes/HistoricReceptions.Designer.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/HistoricReceptions.Designer.cs
@@ -29,30 +29,46 @@
         private void InitializeComponent()
         {
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(150, 23);
+            this.dateTimePicker1.Location = new System.Drawing.Point(240, 30);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 0;
+            // 
+            // Label1
+            // 
+            this.Label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Yellow;
+            this.Label1.Location = new System.Drawing.Point(12, 30);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(227, 19);
+            this.Label1.TabIndex = 35;
+            this.Label1.Text = "Buscar a partir de la fecha:";
             // 
             // HistoricReceptions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(391, 289);
+            this.ClientSize = new System.Drawing.Size(484, 317);
+            this.Controls.Add(this.Label1);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "HistoricReceptions";
             this.Text = "HistoricReceptions";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label Label1;
     }
 }
