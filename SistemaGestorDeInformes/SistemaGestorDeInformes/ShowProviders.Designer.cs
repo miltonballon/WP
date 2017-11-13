1eba58761e0aebd2b61e7cab661d391d8a592611
Pedro <pedroandresmg@gmail.com>
2017-11-12 21:26:29 -0400

    cambiado el tamaño de la interfaz ShowProviders

--- SistemaGestorDeInformes/SistemaGestorDeInformes/ShowProviders.Designer.cs
+++ SistemaGestorDeInformes/SistemaGestorDeInformes/ShowProviders.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ShowProviders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 508);
+            this.Name = "ShowProviders";
             this.Text = "ShowProviders";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
